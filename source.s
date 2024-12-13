96fc9ea2f26d004751d62ad738ab886ab8bcd762
TarekOsama528 <67550435+TarekOsama528@users.noreply.github.com>
2024-12-13 02:08:59 +0200

    RTC + DEFINITIONS

--- source.s
+++ source.s
@@ -12,51 +12,57 @@ __main FUNCTION
 
 
 	;CALL FUNCTION SETUP
-	BL SETUP
-	
-	
-	
-	MOV r0, #0; x1
-	MOV r3, #480; x2
-	MOV r1, #0; y1
-	mov r4, #340; y2
-	mov r10, #BLACK
-	BL DRAW_RECTANGLE_FILLED
-	
-	
-	  ; Draw the vertical lines of 'A'
-    MOV R0, #30        ; X1
-    MOV R1, #60        ; Y1
-    MOV R3, #100        ; X2
-    MOV R4, #180        ; Y2
-    MOV R10, #RED   ; Color (white)
-    BL DRAW_RECTANGLE_FILLED
+;	BL SETUP
+;	
+;	
+;	
+;	MOV r0, #0; x1
+;	MOV r3, #480; x2
+;	MOV r1, #0; y1
+;	mov r4, #340; y2
+;	mov r10, #BLACK
+;	BL DRAW_RECTANGLE_FILLED
+;	
+;	
+;	  ; Draw the vertical lines of 'A'
+;    MOV R0, #30        ; X1
+;    MOV R1, #60        ; Y1
+;    MOV R3, #100        ; X2
+;    MOV R4, #180        ; Y2
+;    MOV R10, #RED   ; Color (white)
+;    BL DRAW_RECTANGLE_FILLED
 
-    MOV R0, #50        ; X1
-    MOV R1, #60        ; Y1
-    MOV R3, #80        ; X2
-    MOV R4, #80        ; Y2
-    MOV R10, #RED   ; Color (white)
-    BL DRAW_RECTANGLE_FILLED
+;    MOV R0, #50        ; X1
+;    MOV R1, #60        ; Y1
+;    MOV R3, #80        ; X2
+;    MOV R4, #80        ; Y2
+;    MOV R10, #RED   ; Color (white)
+;    BL DRAW_RECTANGLE_FILLED
 
-    ; Draw the horizontal line of 'A'
-    MOV R0, #80        ; X1
-    MOV R1, #60        ; Y1
-    MOV R3, #100        ; X2
-    MOV R4, #180     ; Y2
-    MOV R10, #RED   ; Color (white)
-    BL DRAW_RECTANGLE_FILLED
-	
-	
-	MOV R0, #50        ; X1
-    MOV R1, #160        ; Y1
-    MOV R3, #80        ; X2
-    MOV R4, #180     ; Y2
-    MOV R10, #RED   ; Color (white)
-    BL DRAW_RECTANGLE_FILLED
+;    ; Draw the horizontal line of 'A'
+;    MOV R0, #80        ; X1
+;    MOV R1, #60        ; Y1
+;    MOV R3, #100        ; X2
+;    MOV R4, #180     ; Y2
+;    MOV R10, #RED   ; Color (white)
+;    BL DRAW_RECTANGLE_FILLED
+;	
+;	
+;	MOV R0, #50        ; X1
+;    MOV R1, #160        ; Y1
+;    MOV R3, #80        ; X2
+;    MOV R4, #180     ; Y2
+;    MOV R10, #RED   ; Color (white)
+;    BL DRAW_RECTANGLE_FILLED
 
 	;FINAL TODO: DRAW THE ENTIRE SCREEN WITH A CERTAIN COLOR
-
 	;TODO: draw egypt
+	
+	BL RTC_INIT 
+LOOPP
+	LDR R0,=RTC_CNTL
+	LDR R1,[R0]
+	B LOOPP
+	
 	ENDFUNC
     END
